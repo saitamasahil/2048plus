01b766d424125c050bde23bada7041505e9511f7
saitamasahil <sahilmanchanda1996@gmail.com>
2026-08-04 09:54:41 +0530

    Secret Passcode Reveal is now positioned at the very bottom of the store with 5000 coins purchase

--- renderer.lua
+++ renderer.lua
@@ -8778,7 +8778,6 @@ function renderer.getStoreItems()
         {id="extra_bomb",    cost=600,  name="Extra Starting Bomb",  desc="Permanently start Plus Mode with +1 Bomb"},
         {id="jukebox",       cost=900,  name="BGM Jukebox",           desc="Unlock Music Player & Jukebox Control"},
         {id="coin_multiplier",cost=1200,name="2x Coin Multiplier",   desc="Permanently double all earned Coins"},
-        {id="secret_key",    cost=3000, name="Secret Passcode Reveal",desc=(_G.stats and _G.stats.purchased_items and _G.stats.purchased_items["secret_key"]) and "Code: UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A START" or "Unlock master code to access Secret Menu"},
 
         -- Visual FX
         {id="anim_bounce",   cost=500,  name="Bounce Pop FX",        desc="Unlock Bounce Pop Merge FX"},
@@ -8789,7 +8788,10 @@ function renderer.getStoreItems()
         {id="theme_cherry",  cost=1000, name="Cherry Theme",          desc="Unlock sakura blossom theme"},
         {id="theme_gold_luxe",cost=1500,name="Gold Luxe Theme",       desc="Unlock ultra-luxurious gold theme"},
         {id="theme_cyber_grid",cost=1800,name="Cyber Neon Grid Theme",desc="Unlock futuristic cyber grid theme"},
-        {id="theme_synthwave",cost=2000,name="Synthwave 80s Theme",   desc="Unlock retro 80s retrowave theme"}
+        {id="theme_synthwave",cost=2000,name="Synthwave 80s Theme",   desc="Unlock retro 80s retrowave theme"},
+
+        -- Secret Master Code
+        {id="secret_key",    cost=5000, name="Secret Passcode Reveal",desc=(_G.stats and _G.stats.purchased_items and _G.stats.purchased_items["secret_key"]) and "Code: UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A START" or "Unlock master code to access Secret Menu"}
     }
 end
 
